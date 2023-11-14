ff12eb4ad9c47f3b11ed2bbce5181cc96bf840da
anjumvj <anjumvj@gmail.com>
2023-11-14 15:56:53 +0800

    BE,FE,and MYSql

--- backend.sh
+++ backend.sh
@@ -1,7 +1,7 @@
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 dnf install nodejs -y
-
+cp backend.service /etc/systemd/system/backend.service
 useradd expense
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
@@ -9,7 +9,7 @@ cd /app
 unzip /tmp/backend.zip
 cd /app
 npm install
-cp backend.service /etc/systemd/system/backend.service
+
 systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
