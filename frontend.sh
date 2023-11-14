13c4b689c69f8e02f098edc91e075038e50e65bb
anjumvj <anjumvj@gmail.com>
2023-11-14 17:33:04 +0800

    BE,FE,and MYSql

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,22 @@
-echo -e "\e[36m Installing Nginx\e[om"
+echo -e "\e[36m Installing Nginx \e[om"
 dnf install nginx -y
 systemctl enable nginx
+
+echo -e "\e[36m Starting Nginx \e[om"
 systemctl start nginx
+
+echo -e "\e[36m Copy Expense Config file \e[om"
 cp expense.conf /etc/nginx/default.d/expense.conf
+
+echo -e "\e[36m clean old Nginx command \e[om"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[36m Download frontend Application code \e[om"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[36m Extract downloaded Application content code \e[om"
 cd /usr/share/nginx/html
+
 unzip /tmp/frontend.zip
 
 systemctl restart nginx
