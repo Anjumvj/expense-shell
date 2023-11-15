c90c97f41a5c4a9b43afee61eff23a4fb1e863e1
anjumvj <anjumvj@gmail.com>
2023-11-15 15:28:39 +0800

    BE,FE,and MYSql

--- backend.sh
+++ backend.sh
@@ -1,19 +1,19 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
-dnf install nodejs -y
-cp backend.service /etc/systemd/system/backend.service
+dnf module disable nodejs -y &>>/tmp/expense.log
+dnf module enable nodejs:18 -y &>>/tmp/expense.log
+dnf install nodejs -y &>>/tmp/expense.log
+cp backend.service /etc/systemd/system/backend.service &>>/tmp/expense.log
 useradd expense
 mkdir /app
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>/tmp/expense.log
 cd /app
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip &>>/tmp/expense.log
 cd /app
 npm install
-dnf install mysql -y
+dnf install mysql -y &>>/tmp/expense.log
 mysql -h mysql-dev.anjumdevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
-systemctl daemon-reload
-systemctl enable backend
-systemctl restart backend
+systemctl daemon-reload &>>/tmp/expense.log
+systemctl enable backend &>>/tmp/expense.log
+systemctl restart backend &>>/tmp/expense.log
 
 
 
