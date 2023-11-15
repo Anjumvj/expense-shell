c4b21eff4e5c3c70c58c04aca5f2ac5cd62b7e72
anjumvj <anjumvj@gmail.com>
2023-11-15 15:34:31 +0800

    BE,FE,and MYSql

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,18 @@
+
+echo -e "\e[36m Enable nodejs \e[om"
 dnf module disable mysql -y
+
+echo -e "\e[36m Enable nodejs \e[om"
 cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m Installing mysql \e[om"
 dnf install mysql-community-server -y
+
+echo -e "\e[36m Enable mysql \e[om"
 systemctl enable mysqld
+
+echo -e "\e[36m start nodejs \e[om"
 systemctl start mysqld
+
+echo -e "\e[36m password \e[om"
 mysql_secure_installation --set-root-pass ExpenseApp@1
