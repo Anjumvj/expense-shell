37ac50bc7d37d67f6f6eddc110df8edd7ec99913
anjumvj <anjumvj@gmail.com>
2023-11-16 13:19:07 +0800

    BE,FE,and MYSql

--- backend.sh
+++ backend.sh
@@ -7,7 +7,6 @@ if[ $? -eq 0 ]; then
   echo -e "\e[36m SUCCESS \e[om"
 else
   echo -e "\e[36m FAILURE \e[om"
-echo $?
 
 echo -e "\e[36m Enable nodejs 18 version  \e[om"
 dnf module enable nodejs:18 -y &>>$log_file
@@ -15,7 +14,6 @@ if[ $? -eq 0 ]; then
   echo -e "\e[36m SUCCESS \e[om"
 else
   echo -e "\e[36m FAILURE \e[om"
-echo $?
 
 echo -e "\e[36m Installing nodejs \e[om"
 dnf install nodejs -y&>>$log_file
@@ -23,8 +21,6 @@ if[ $? -eq 0 ]; then
   echo -e "\e[36m SUCCESS \e[om"
 else
   echo -e "\e[36m FAILURE \e[om"
-echo $?
-
 
 echo -e "\e[36m copy backend service file \e[om"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
@@ -32,7 +28,7 @@ if[ $? -eq 0 ]; then
   echo -e "\e[36m SUCCESS \e[om"
 else
   echo -e "\e[36m FAILURE \e[om"
-echo $?
+
 
 echo -e "\e[36m Add Application User \e[om"
 useradd expense &>>$log_file
@@ -40,7 +36,6 @@ if[ $? -eq 0 ]; then
   echo -e "\e[36m SUCCESS \e[om"
 else
   echo -e "\e[36m FAILURE \e[om"
-echo $?
 
 echo -e "\e[36m Create Directory \e[om"
 mkdir /app &>>$log_file
@@ -48,7 +43,6 @@ if[ $? -eq 0 ]; then
   echo -e "\e[36m SUCCESS \e[om"
 else
   echo -e "\e[36m FAILURE \e[om"
-echo $?
 
 echo -e "\e[36m Delete Old Application Content \e[om"
 rm -rf /app/* &>>$log_file
@@ -56,8 +50,6 @@ if[ $? -eq 0 ]; then
   echo -e "\e[36m SUCCESS \e[om"
 else
   echo -e "\e[36m FAILURE \e[om"
-echo $?
-
 
 echo -e "\e[36m Download Application Content \e[om"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
@@ -65,8 +57,6 @@ if[ $? -eq 0 ]; then
   echo -e "\e[36m SUCCESS \e[om"
 else
   echo -e "\e[36m FAILURE \e[om"
-echo $?
-
 
 echo -e "\e[36m Extract Application Content \e[om"
 cd /app &>>$log_file
@@ -75,7 +65,6 @@ if[ $? -eq 0 ]; then
   echo -e "\e[36m SUCCESS \e[om"
 else
   echo -e "\e[36m FAILURE \e[om"
-echo $?
 
 echo -e "\e[36m Download NodeJs Dependencies \e[om"
 npm install &>>$log_file
@@ -83,7 +72,6 @@ if[ $? -eq 0 ]; then
   echo -e "\e[36m SUCCESS \e[om"
 else
   echo -e "\e[36m FAILURE \e[om"
-echo $?
 
 echo -e "\e[36m Installing mysql client to Load schema \e[om"
 dnf install mysql -y &>>$log_file
@@ -91,7 +79,6 @@ if[ $? -eq 0 ]; then
   echo -e "\e[36m SUCCESS \e[om"
 else
   echo -e "\e[36m FAILURE \e[om"
-echo $?
 
 echo -e "\e[36m Load schema \e[om"
 mysql -h mysql-dev.anjumdevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
@@ -99,13 +86,15 @@ if[ $? -eq 0 ]; then
   echo -e "\e[36m SUCCESS \e[om"
 else
   echo -e "\e[36m FAILURE \e[om"
-echo $?
 
 echo -e "\e[36m Starting backend service \e[om"
 systemctl daemon-reload &>>/tmp/expense.log &>>$log_file
 systemctl enable backend &>>/tmp/expense.log &>>$log_file
 systemctl restart backend &>>/tmp/expense.log &>>$log_file
-echo $?
+if[ $? -eq 0 ]; then
+  echo -e "\e[36m SUCCESS \e[om"
+else
+  echo -e "\e[36m FAILURE \e[om"
 
 
 
